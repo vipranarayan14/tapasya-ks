504fb6995be459791b3edf3092008b7fda09e0b0
vipranarayan14 <vipranarayan14@gmail.com>
2017-05-22 21:02:32 +0530

    improve ui

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const navBtns = document.querySelectorAll('nav .w3-bar-item');
+const navBtns = document.querySelectorAll('header a');
 const allSections = document.querySelectorAll('section');
 const allSubpages = document.querySelectorAll('.sub-page');
+const allDropdowns = document.querySelectorAll('.w3-dropdown-hover');
 
 for (let i = 0, length = navBtns.length; i < length; i++) {
 
   navBtns[i].addEventListener('click', (e) => {
 
     e.preventDefault();
+    highlightNavBtn(navBtns[i]);
     showElement(navBtns[i].getAttribute('href'));
   })
 }
@@ -16,7 +18,7 @@ function showElement(eleId) {
   const ele = document.querySelector(eleId);
 
   if (ele) {
-    
+
     hideAllPagesAndSubpages();
 
     const eleParent = ele.parentElement;
@@ -40,5 +42,28 @@ function hideAllPagesAndSubpages() {
   for (let i = 0, length = allSubpages.length; i < length; i++) {
 
     allSubpages[i].style.display = 'none';
+  }
+}
+
+function highlightNavBtn(ele) {
+
+  const headerActiveLinks = document.querySelectorAll('header .w3-green');
+
+  for (let i = 0, length = headerActiveLinks.length; i < length; i++) {
+
+    headerActiveLinks[i].classList.remove('w3-green');
+  }
+
+  ele.classList.add('w3-green');
+
+  for (let i = 0, length = allDropdowns.length; i < length; i++) {
+
+    const isActive = allDropdowns[i].querySelector('.w3-green') !== null;
+
+    if(isActive) {
+
+      allDropdowns[i].querySelector('button').classList.add('w3-green');
+    }
+    
   }
 }
