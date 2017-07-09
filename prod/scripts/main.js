d067199401faf74e648f7d482c2f07474db214f4
vipranarayan14 <vipranarayan14@gmail.com>
2017-07-09 19:35:32 +0530

    remove commented and unnecessary codes.

--- prod/scripts/main.js
+++ prod/scripts/main.js
@@ -1,105 +1,36 @@
-const navBtns = document.querySelectorAll('header a');
-const allPages = document.querySelectorAll('.page');
-const allSubpages = document.querySelectorAll('.sub-page');
+const navBtns = document.querySelectorAll('.navLink');
 const allDropdowns = document.querySelectorAll('.p-dropdown');
 const menuIcon = document.querySelector('.menu-icon');
-const hiliteClass = "w3-green";
+let mediaMatch = window.matchMedia("(min-width:700px)");
 
-// window.addEventListener('load', () => {
-//   const reqHash = location.hash;
-//   if (reqHash) {
-//     showElement(reqHash);
-//   }
-// });
+for (let i = 0, length = navBtns.length; i < length; i++) {
 
-// for (let i = 0, length = navBtns.length; i < length; i++) {
+  navBtns[i].addEventListener('click', (e) => {
 
-//   navBtns[i].addEventListener('click', (e) => {
-
-//     e.preventDefault();
-//     highlightNavBtn(navBtns[i]);
-//     showElement(navBtns[i].getAttribute('href'));
-
-//     if (!window.matchMedia("(min-width:700px)").matches) {
-//       toggleNav();
-//     }
-//   });
-// }
+    // if (!mediaMatch.matches) {
+    toggleNav();
+    // }
+  });
+}
 
 for (let i = 0, length = allDropdowns.length; i < length; i++) {
 
   allDropdowns[i].querySelector('.p-dropdown-btn').addEventListener('click', (e) => {
 
-    e.preventDefault();
-    // resetAllDDs();
     e.target.classList.toggle('active');
     allDropdowns[i].querySelector('.p-dropdown-content').classList.toggle('active');
   });
 }
-// menuIcon.addEventListener('click', (e) => {
-//   e.preventDefault();
-//   toggleNav();
-// });
+menuIcon.addEventListener('click', (e) => {
 
-// function toggleNav() {
-//   document.querySelector('header').classList.toggle('nav-open');
-//   document.querySelector('nav').classList.toggle('nav-open');
-// }
-// function showElement(eleId) {
+  e.preventDefault();
+  toggleNav();
+});
 
-//   const ele = document.querySelector(eleId);
+function toggleNav() {
 
-//   if (ele) {
+  document.querySelector('header').classList.toggle('nav-open');
+  document.querySelector('nav').classList.toggle('nav-open');
+}
 
-//     hideAllPagesAndSubpages();
-
-//     const eleParent = ele.parentElement;
-
-//     if (eleParent.nodeName === 'SECTION' && eleParent.style.display === 'none') {
-
-//       eleParent.style.display = 'block';
-//     }
-
-//     ele.style.display = 'block';
-//   }
-// }
-
-// function hideAllPagesAndSubpages() {
-
-//   for (let i = 0, length = allPages.length; i < length; i++) {
-
-//     allPages[i].style.display = 'none';
-//   }
-
-//   for (let i = 0, length = allSubpages.length; i < length; i++) {
-
-//     allSubpages[i].style.display = 'none';
-//   }
-// }
-
-// function highlightNavBtn(ele) {
-
-//   const headerActiveLinks = document.querySelectorAll('header .' + hiliteClass);
-
-//   for (let i = 0, length = headerActiveLinks.length; i < length; i++) {
-
-//     headerActiveLinks[i].classList.remove(hiliteClass);
-//   }
-
-//   if (!ele.classList.contains('logo-text')) {
-
-//     ele.classList.add(hiliteClass);
-
-//     for (let i = 0, length = allDropdowns.length; i < length; i++) {
-
-//       const isActive = allDropdowns[i].querySelector('.' + hiliteClass) !== null;
-
-//       if (isActive) {
-
-//         allDropdowns[i].querySelector('.p-dropdown-btn').classList.add(hiliteClass);
-//       }
-
-//     }
-//   }
-// }
 initHashRouting('sub-page');
