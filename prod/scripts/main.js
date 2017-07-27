ed2d9af917398081da8dd6daaa887d73cb42070a
vipranarayan14 <vipranarayan14@gmail.com>
2017-07-27 22:01:26 +0530

    make '.logo-text' hide in '#home' view

--- prod/scripts/main.js
+++ prod/scripts/main.js
@@ -1,6 +1,7 @@
 const navBtns = document.querySelectorAll('.navLink');
 const allDropdowns = document.querySelectorAll('.p-dropdown');
 const menuIcon = document.querySelector('.menu-icon');
+const logoText = document.querySelector('.logo .logo-text');
 let mediaMatch = window.matchMedia("(min-width:700px)");
 
 for (let i = 0, length = navBtns.length; i < length; i++) {
@@ -21,6 +22,19 @@ for (let i = 0, length = allDropdowns.length; i < length; i++) {
     allDropdowns[i].querySelector('.p-dropdown-content').classList.toggle('active');
   });
 }
+
+window.addEventListener('load', toggleLogoText);
+window.addEventListener('hashchange', toggleLogoText);
+
+function toggleLogoText() {
+
+  if (location.hash === '' || location.hash === '#home') {
+    logoText.style.display = 'none';
+  } else {
+    logoText.style.display = 'inline-block';
+  }
+}
+
 menuIcon.addEventListener('click', (e) => {
 
   e.preventDefault();
