465e8765d56a04fbbaec06d89120d5f6d43ee54e
vipranarayan14 <vipranarayan14@gmail.com>
2017-07-29 20:41:20 +0530

    use 'w3-hide' class instead of 'ele.style.display'  for hiding logo-text.

--- prod/scripts/main.js
+++ prod/scripts/main.js
@@ -2,15 +2,18 @@ const navBtns = document.querySelectorAll('.navLink');
 const allDropdowns = document.querySelectorAll('.p-dropdown');
 const menuIcon = document.querySelector('.menu-icon');
 const logoText = document.querySelector('.logo .logo-text');
-let mediaMatch = window.matchMedia("(min-width:700px)");
+
+menuIcon.addEventListener('click', (e) => {
+
+  e.preventDefault();
+  toggleNav();
+});
 
 for (let i = 0, length = navBtns.length; i < length; i++) {
 
   navBtns[i].addEventListener('click', (e) => {
 
-    // if (!mediaMatch.matches) {
     toggleNav();
-    // }
   });
 }
 
@@ -23,23 +26,18 @@ for (let i = 0, length = allDropdowns.length; i < length; i++) {
   });
 }
 
+// Hide Logo-text in '#home' page
 window.addEventListener('load', toggleLogoText);
 window.addEventListener('hashchange', toggleLogoText);
 
 function toggleLogoText() {
 
   if (location.hash === '' || location.hash === '#home') {
-    logoText.style.display = 'none';
+    logoText.classList.add('w3-hide');
   } else {
-    logoText.style.display = 'inline-block';
+    logoText.classList.remove('w3-hide');
   }
 }
-
-menuIcon.addEventListener('click', (e) => {
-
-  e.preventDefault();
-  toggleNav();
-});
 
 function toggleNav() {
 
