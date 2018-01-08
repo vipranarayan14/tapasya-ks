7d1c4557c9a7a242a18c2b00fa2db4a4b60adaf5
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-01-08 17:45:00 +0530

    testing deployment of 'feature' branch part2: use another 'elif' as '-o' did not work.

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,28 @@
- WORKING_DIR="./dist";
+WORKING_DIR="./dist";
 
-file_deploy() {  
-
-  if [ "$TRAVIS_BRANCH" == "master" -o "$TRAVIS_BRANCH" == "feature"]; then
-    REMOTE_DIR="~/public_html";
-  elif [ "$TRAVIS_BRANCH" == "development" ]; then
-    REMOTE_DIR="~/public_html_alpha";
-  else 
-    echo "This will not deploy!";
-    exit 0;
-  fi
-
-  lftp -u $FTP_USER,$FTP_PASS -e "set ftp:ssl-allow no ; mirror -v -c -e -R $WORKING_DIR $REMOTE_DIR ; exit" -p 21 $FTP_HOST
+file_deploy() {
+    
+    if [ "$TRAVIS_BRANCH" == "master"]; then
+       
+        REMOTE_DIR="~/public_html";
+       
+        elif [ "$TRAVIS_BRANCH" == "feature" ]; then
+       
+        REMOTE_DIR="~/public_html";
+        
+        elif [ "$TRAVIS_BRANCH" == "development" ]; then
+        
+        REMOTE_DIR="~/public_html_alpha";
+    else
+        echo "This will not deploy!";
+        exit 0;
+    fi
+    
+    lftp -u $FTP_USER,$FTP_PASS -e "set ftp:ssl-allow no ; mirror -v -c -e -R $WORKING_DIR $REMOTE_DIR ; exit" -p 21 $FTP_HOST
 }
 
 os_deploy() {
-  
-  npm install;
-  . build.sh;
+    
+    npm install;
+    . build.sh;
 }
