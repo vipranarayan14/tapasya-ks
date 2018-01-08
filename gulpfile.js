1e16f326fb44e74769d52fb08436d04c562ace43
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-01-08 21:35:44 +0530

    make gulp to copy dot folders also during 'copy-others' task.

--- gulpfile.js
+++ gulpfile.js
@@ -99,7 +99,12 @@ gulp.task('copy-others', function () {
   gulp.src('./prod/images/**/*')
     .pipe(gulp.dest('./dist/images'));
 
-  return gulp.src('./prod/others/**/*')
+  return gulp.src(
+    ['./prod/others/**/*'],
+    {
+      dot: true
+    }
+  )
     .pipe(gulp.dest('./dist'));
 });
 
