59b2a16dca717185f91dce6019afb498c7214789
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-02-25 21:30:26 +0530

    use new vShowBox.

--- prod/pages/gallery/vshowbox.config.js
+++ prod/pages/gallery/vshowbox.config.js
@@ -1,3 +1,56 @@
 import { vShowBox } from 'vshowbox';
+console.log(document.querySelector('#vShowBox'));
+vShowBox({
 
-vShowBox.init();
+  container: document.querySelector('#vShowBox'),
+
+  slides: [
+
+    {
+      caption: 'Indu Varma with Smt. Kamala Lakshman at the Rotary conference Columbu.',
+      content: 'images/main/gallery/0002IMG.jpg'
+    },
+    {
+      caption: 'Tapasya\'s first annversy function;  Janardhanan felicitates Guru Kittappapillai.',
+      content: 'images/main/gallery/0004IMG.jpg'
+    },
+    {
+      caption: 'Smt. T. Mukta performing at a Tapasya function.',
+      content: 'images/main/gallery/0005IMG.jpg'
+    },
+    {
+      caption: 'Documentation of Tyagesh Kuravanji with T R Tilakam, a Padmashree awardee, for the South Zone Cultural Centre.',
+      content: 'images/main/gallery/0006IMG.jpg'
+    },
+    {
+      caption: 'Pudukottai paramparyam from Marainduvaraum Marabugal.',
+      content: 'images/main/gallery/0007IMG.jpg'
+    },
+    {
+      caption: 'Former Governor of Tamilnadu releases Marainduvarum Marabugal.',
+      content: 'images/main/gallery/0008IMG.jpg'
+    },
+    {
+      caption: 'Former Governor of Tamilnadu felicitates Sri Subbaraya Pillai.',
+      content: 'images/main/gallery/0009IMG.jpg'
+    },
+    {
+      caption: 'Former Governor of Tamilnadu felicitates Smt. T R Tilakam.',
+      content: 'images/main/gallery/0010IMG.jpg'
+    },
+    {
+      caption: 'Tyagesh Kuravanji.',
+      content: 'images/main/gallery/0003IMG.jpg'
+    },
+    {
+      caption: 'Indu Varma at a LecDem on rare Tanjore compositions.',
+      content: 'images/main/gallery/0011IMG.jpg'
+    },
+    {
+      caption: '',
+      content: 'images/main/gallery/0001IMG.jpg'
+    }
+
+  ]
+
+});
