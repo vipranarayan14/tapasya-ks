f9f2f3d98a0450a0d0073c7766e45ce319f1ee18
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2018-02-28 18:25:54 +0530

    correct typos.

--- prod/pages/gallery/vshowbox.config.js
+++ prod/pages/gallery/vshowbox.config.js
@@ -7,12 +7,12 @@ vShowBox({
   slides: [
 
     {
-      caption: 'Indu Varma with Smt. Kamala Lakshman at the Rotary conference Columbu.',
+      caption: 'Indu Varma with Smt. Kamala Lakshman at the Rotary Conference Columbu.',
       content: 'images/gallery/slide/0002IMG-slide.jpg',
       preview: 'images/gallery/preview/0002IMG-preview.jpg'
     },
     {
-      caption: 'Tapasya\'s first annversy function;  Janardhanan felicitates Guru Kittappapillai.',
+      caption: 'Tapasya\'s first anniversary function;  Janardhanan felicitates Guru Kittappapillai.',
       content: 'images/gallery/slide/0004IMG-slide.jpg',
       preview: 'images/gallery/preview/0004IMG-preview.jpg'
     },
@@ -32,17 +32,17 @@ vShowBox({
       preview: 'images/gallery/preview/0007IMG-preview.jpg'
     },
     {
-      caption: 'Former Governor of Tamilnadu releases Marainduvarum Marabugal.',
+      caption: 'Former Governor of Tamil Nadu releases Marainduvarum Marabugal.',
       content: 'images/gallery/slide/0008IMG-slide.jpg',
       preview: 'images/gallery/preview/0008IMG-preview.jpg'
     },
     {
-      caption: 'Former Governor of Tamilnadu felicitates Sri Subbaraya Pillai.',
+      caption: 'Former Governor of Tamil Nadu felicitates Sri Subbaraya Pillai.',
       content: 'images/gallery/slide/0009IMG-slide.jpg',
       preview: 'images/gallery/preview/0009IMG-preview.jpg'
     },
     {
-      caption: 'Former Governor of Tamilnadu felicitates Smt. T R Tilakam.',
+      caption: 'Former Governor of Tamil Nadu felicitates Smt. T R Tilakam.',
       content: 'images/gallery/slide/0010IMG-slide.jpg',
       preview: 'images/gallery/preview/0010IMG-preview.jpg'
     },
@@ -52,7 +52,7 @@ vShowBox({
       preview: 'images/gallery/preview/0003IMG-preview.jpg'
     },
     {
-      caption: 'Indu Varma at a LecDem on rare Tanjore compositions.',
+      caption: 'Indu Varma at a Lec-Dem on rare Tanjore compositions.',
       content: 'images/gallery/slide/0011IMG-slide.jpg',
       preview: 'images/gallery/preview/0011IMG-preview.jpg'
     },
